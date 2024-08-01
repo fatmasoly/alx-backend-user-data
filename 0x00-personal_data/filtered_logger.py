2d6a2daafda49ca08893e980f666bb1d8a6b739b
fatma soliman <fatmacivil@hotmail.com>
2024-08-01 05:18:12 +0300

    first commit

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" Module that contains a filter_datum function """
+
+import re
+from typing import List
+
+
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+    """ Returns the log message obfuscated """
+    for field in fields:
+        message = re.sub(rf'{field}=(.*?){separator}',
+                         f'{field}={redaction}{separator}', message)
+    return message
