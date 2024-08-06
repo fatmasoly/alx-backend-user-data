f0a6c42048e576c040a65a5af739b5c2268eb620
fatma soliman <fatmacivil@hotmail.com>
2024-08-06 11:25:44 +0300

    update methods

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -2,6 +2,7 @@
 """ Basic authentication"""
 
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -19,3 +20,15 @@ class BasicAuth(Auth):
         if not authorization_header.startswith('Basic '):
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(
+                    self, base64_authorization_header: str) -> str:
+        """ Decodes a base64 string"""
+        if base64_authorization_header is None or type(
+                base64_authorization_header) is not str:
+            return None
+        try:
+            return base64.b64decode(
+                base64_authorization_header).decode('utf-8')
+        except Exception:
+            return None
