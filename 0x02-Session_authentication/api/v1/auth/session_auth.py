95f5990fa64ac67a48f3f685352045fab0f505d8
fatma soliman <fatmacivil@hotmail.com>
2024-08-08 13:22:50 +0300

    fix code

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -15,12 +15,12 @@ class SessionAuth(Auth):
         super().__init__()
 
 
-def create_session(self, user_id: str = None) -> str:
-    """ Create a session"""
-    if not user_id or not isinstance(user_id, str):
-        return None
+    def create_session(self, user_id: str = None) -> str:
+        """ Create a session"""
+        if not user_id or not isinstance(user_id, str):
+            return None
 
-    session_id = str(uuid.uuid4())
-    self.user_id_by_session_id[session_id] = user_id
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
 
-    return session_id
+        return session_id
