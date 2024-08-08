6baf63a00934cb37e83a4625a73c6b007cde4559
fatma soliman <fatmacivil@hotmail.com>
2024-08-08 14:08:45 +0300

    error

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -69,13 +69,10 @@ def before_request():
             and auth.session_cookie(request) is None):
         abort(401)
 
-    if auth.authorization_header(request) is None:
-        abort(401)
-
     if auth.current_user(request) is None:
         abort(403)
-    else:
-        request.current_user = auth.current_user(request)
+
+    request.current_user = auth.current_user(request)
 
 
 if __name__ == "__main__":
