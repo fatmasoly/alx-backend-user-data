526e474c8a3166b124343ad9b8a67b3f21426aa1
fatma soliman <fatmacivil@hotmail.com>
2024-08-14 22:31:47 +0300

    2nd route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,14 +2,30 @@
 """ Flask app"""
 
 from flask import Flask, jsonify
+from auth import Auth
 
+
+AUTH = Auth()
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'], strict_slashes=False)
-def hello() -> str:
+def index() -> str:
     """ single GET route  """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'], strict_slashes=False)
+def users() -> str:
+    """ POST /users route """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({'email': user.email, 'message': 'user created'})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
