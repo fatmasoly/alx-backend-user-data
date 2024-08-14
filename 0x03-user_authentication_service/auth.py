38aaf66badaa8ee064b052a036b3b083a0445835
fatma soliman <fatmacivil@hotmail.com>
2024-08-14 22:46:30 +0300

    add uuid for session

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -35,6 +35,16 @@ class Auth:
         except NoResultFound:
             return False
 
+    def create_session(self, email: str) -> str:
+        """ Create a new session for a user"""
+        try:
+            user = self._db.find_user_by(email=email)
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+        except NoResultFound:
+            return None
+
 
 def _hash_password(password: str) -> bytes:
     """ Returns a salted hash of the input password"""
