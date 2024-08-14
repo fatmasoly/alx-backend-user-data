7c98765c859ab09446c6ef26773ff6f4af94a325
fatma soliman <fatmacivil@hotmail.com>
2024-08-14 22:40:48 +0300

    login

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -25,6 +25,15 @@ class Auth:
                                      hashed_password=hashed_password)
             return user
 
+    def valid_login(self, email: str, password: str) -> bool:
+        """ Validate user credentials"""
+        try:
+            user = self._db.find_user_by(email=email)
+            return bcrypt.checkpw(password.encode('utf-8'),
+                                  user.hashed_password)
+        except NoResultFound:
+            return False
+
 
 def _hash_password(password: str) -> bytes:
     """ Returns a salted hash of the input password"""
