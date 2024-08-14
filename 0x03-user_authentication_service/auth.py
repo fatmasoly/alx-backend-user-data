e45b35597ae0090ea82d894525a6919c813dba3a
fatma soliman <fatmacivil@hotmail.com>
2024-08-14 23:06:31 +0300

    fix auth

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -45,6 +45,48 @@ class Auth:
         except NoResultFound:
             return None
 
+    def get_user_from_session_id(self, session_id: str) -> User:
+        """ Get a user from a session ID"""
+        if not session_id:
+            return None
+
+        try:
+            user = self._db.find_user_by(session_id=session_id)
+            return user
+        except NoResultFound:
+            return None
+
+    def destroy_session(self, user_id: int) -> None:
+        """ Destroy a session"""
+        if not user_id:
+            return None
+
+        try:
+            user = self._db.update_user(user_id, session_id=None)
+        except ValueError:
+            return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """ Generate a reset password token"""
+        try:
+            user = self._db.find_user_by(email=email)
+            reset_token = _generate_uuid()
+            self._db.update_user(user.id, reset_token=reset_token)
+            return reset_token
+        except NoResultFound:
+            raise ValueError
+
+    def update_password(self, reset_token: str, password: str) -> None:
+        """ Update user password"""
+        try:
+            user = self._db.find_user_by(reset_token=reset_token)
+            new_hashed_password = _hash_password(password)
+            self._db.update_user(user.id,
+                                 hashed_password=new_hashed_password,
+                                 reset_token=None)
+        except NoResultFound:
+            raise ValueError()
+
 
 def _hash_password(password: str) -> bytes:
     """ Returns a salted hash of the input password"""
